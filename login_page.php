da949720d8501d85ad07e519821432f053c781d9
[Rachita Maharjan] <[smilerac15@gmail.com]>
2017-05-23 00:33:35 +0545

    form

--- login_page.php
+++ login_page.php
@@ -3,11 +3,12 @@
 		<img src="logo.jpg" height="200" width="100" align="left">
 		<H1>HIMALAYAN CARAVAN</H1></br>
 		Please log in to continue:</br>
-		<form>
-			Username: &nbsp<input type="text" name="text"></br>
+		<form action ="Home.php" method="post" enctype="multipart/form-data">
+			Username: &nbsp<input type="text" name="username"></br>
 			Password: &nbsp<input type="password" name="password"></br>
 			<input type="submit" name="submit" value="Submit">
 			<input type="submit" name="cancel" value="Cancel"></br>
+
 
 
 		
