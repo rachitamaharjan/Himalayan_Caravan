03ec8ec18f682726186b383827bb96d5340f8a96
[Rachita Maharjan] <[smilerac15@gmail.com]>
2017-05-22 23:46:45 +0545

    created form for login page

--- login_page.php
+++ login_page.php
@@ -1,7 +1,13 @@
 <html>
 	<body>
-	<imgsrc="logo.jpg" height='200' width='100' align='left'>
-	<H1>HIMALAYAN CARAVAN</H1>
+		<img src="logo.jpg" height="200" width="100" align="left">
+		<H1>HIMALAYAN CARAVAN</H1></br>
+		Please log in to continue:</br>
+		<form>
+			Username: &nbsp<input type="text" name="text"></br>
+			Password: &nbsp<input type="password" name="password"></br>
+		
+		</form>
 
 
 	</body>
