6e07e8e8e59babef0e95865b7889e4c20eb80a4c
[Rachita Maharjan] <[smilerac15@gmail.com]>
2017-05-23 14:46:37 +0545

    home.php, login_page.php added and hyperlinks added

--- Home.php
+++ Home.php
@@ -1,12 +1,31 @@
 <html>
 	<body>
-		<?php
-			$a="blah";
-			echo $a;
-			echo "</br>";
-			echo $a ."smilerac";
+		<form>
+			<table border=1>
+				<tr>
+					<td><a href="Home.php">Home</a></td>
+					<td><a href="About_us.php">About Us</a></td>
+					<td><a href="Shop.php">Shop</a></td>
+					<td><a href="Support_us.php">Support Us</a></td>
+					<td><a href="Contact_us.php">Contact Us</a></td>
+					<td><a href="login_page.php">Login/Sign up</a></td>
+				</tr>
+			</table>
+		</form>
 
-		?>
+
+
+		<?php
 		
+			
+			if (isset($_POST["submit"]))
+			{
+				echo "Welcome &nbsp" . $_POST["namevalue"];
+			}
+			else
+			{
+				echo ($_SERVER["PHP_SELF"]);
+			}
+		?>		
 	</body>
 </html>
