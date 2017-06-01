1bdb63ca32d9e52e4d8ca4d32989dd080f1fd2cb
[Rachita Maharjan] <[smilerac15@gmail.com]>
2017-06-01 17:22:54 +0545

    framework.css add, worked on slide

--- Home.php
+++ Home.php
@@ -33,7 +33,7 @@ p.topic {
 		<title> Online Food System</title> 
 		<link rel="stylesheet" type="text/css" href="style.css">
 		<link href="https://fonts.googleapis.com/css?family=Roboto:100" type="text/css" rel="stylesheet">
-		<link rel="stylesheet" type="text/css" href="stylesheets/framework_file.css">
+		<link rel="stylesheet" type="text/css" href="framework.css">
 	</head>  
 	<body>
 	<?php
@@ -54,13 +54,48 @@ p.topic {
 
 		</div>
 	<//image slider>
-		<div class="slideshow-container">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+
+<!--<h2 class="w3-center">Manual Slideshow</h2>!-->
+
+<div class="w3-content w3-display-container">
+  <img class="mySlides" src="mangoes.jpg" style="width:100%">
+  <img class="mySlides" src="potatoes.jpg" style="width:100%">
+  <img class="mySlides" src="carrots.jpg" style="width:100%">
+  <!img class="mySlides" src="img_forest.jpg" style="width:100%">
+
+  <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
+  <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
+</div>
+
+<script>
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+</script>
+		<!-- <div class="slideshow-container">
   <div class="mySlides fade">
     <div class="numbertext">1 / 3</div>
     <img src="mangoes.jpg" style="width:100%">
     <div class="text">Mangoes</div>
   </div> 
-   <div class="mySlides fade">
+   <div class="mySlies fade">
     <div class="numbertext">2 / 3</div>
     <img src="potatoes.jpg" style="width:100%">
     <div class="text">Potatoes</div>
@@ -81,9 +116,9 @@ p.topic {
   <span class="dot" onclick="currentSlide(1)"></span> 
   <span class="dot" onclick="currentSlide(2)"></span> 
   <span class="dot" onclick="currentSlide(3)"></span> 
-</div>
-  		<//image slider>
-	</form>
+</div> !-->
+  		<//image slider
+>	</form>
 		
 		
 		
